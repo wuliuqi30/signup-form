07caae4d63afa4168af6fa28b46407bd5e3dbe62
Hersh <sanganihersh@gmail.com>
2024-07-28 21:46:33 -0400

    Add validities to name

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 // Scripts for my code
+
+console.log("Hello World")
+
+
+const firstName = document.getElementById("first-name");
+
+firstName.addEventListener("input",(event)=>{
+
+    if (firstName.validity.typeMismatch) {
+        firstName.setCustomValidity("1-20 chars, at least 1 letter");
+      } else {
+        firstName.setCustomValidity("");
+      }
+
+})
